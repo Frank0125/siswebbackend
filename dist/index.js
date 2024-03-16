0e88e594da43be0357aca3f3d9e3fca50ab74a15
Frank0125 <a01252974@tec.mx>
2024-03-15 17:25:04 -0700

    Fix: can connect to database

--- dist/index.js
+++ dist/index.js
@@ -7,7 +7,19 @@ const express_1 = __importDefault(require("express"));
 const routes_1 = __importDefault(require("./src/routes"));
 const app = (0, express_1.default)();
 const morgan = require('morgan');
+const db = require('./src/models');
 const port = 3000;
+db.sequelize.sync()
+    .then(() => {
+    console.log("Synced db.");
+})
+    .catch((err) => {
+    console.log("Failed to sync db: " + err.message);
+});
+// db.sequelize.sync({ force: true })
+//     .then(() => {
+//     console.log("Drop and re-sync db.");
+// });
 app.use(morgan('dev'));
 app.use(express_1.default.json());
 app.use(routes_1.default);
--- index.ts
+++ index.ts
@@ -2,10 +2,26 @@ import express, { Express, Request, Response } from "express";
 import apiRouter from './src/routes';
 const app: Express = express();
 const morgan = require('morgan');
+const db = require('./src/models')
+    
 const port = 3000;
+
+db.sequelize.sync()
+    .then(() => {
+        console.log("Synced db.");
+    })
+    .catch((err:Error) => {
+        console.log("Failed to sync db: " + err.message);
+    });
+
+db.sequelize.sync({ force: true })
+    .then(() => {
+        console.log("Drop and re-sync db.");
+    });
+   
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(apiRouter);
 app.listen(port, () => {
-console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
